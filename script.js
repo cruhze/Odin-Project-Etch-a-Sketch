1f1991c41b16fa8d1664f60d9074b7421b8c61d4
cruhze <cruhze@gmail.com>
2024-03-14 21:23:04 -0400

    Etch-a-sketch v1 final

---
+++ script.js
@@ -0,0 +1,30 @@
+const gridContainer = document.getElementById('gridContainer');
+const gridSizeInput = document.getElementById('gridSize');
+
+function generateGrid() {
+    let gridSize = parseInt(gridSizeInput.value);
+    gridSize = Math.min(gridSize, 100);
+    clearGrid();
+    createGrid(gridSize); 
+}
+
+function createGrid(gridSize) { 
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridItem.style.width = `calc(100% / ${gridSize})`; 
+        gridItem.style.height = `calc(100% / ${gridSize})`;
+        gridItem.addEventListener('mouseenter', function() {
+            this.classList.add('black-grid-item');
+        });
+        gridContainer.appendChild(gridItem);
+    }
+}
+
+function clearGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+generateGrid();
